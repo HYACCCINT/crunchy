c2d6b7ad75a42d5ac62bb7ba693b56152a209257
Spyguy001 <amritpal.aujla@mail.utoronto.ca>
2020-10-17 00:48:44 -0400

    Added endpoints for basic SDCFormManager functionality

--- sdc-form-manager/server/server.ts
+++ sdc-form-manager/server/server.ts
@@ -7,12 +7,45 @@ const port = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+app.get('/SDC_Form', async (req, res, next) => {
+  const procedureID = req.query.procedureID;
+  if(!procedureID){
+    next(new Error("procedureID did not show in parameters"));
+  }
+  const form = { formID: procedureID, attributes: "Blahblah" }; //do call for database function here
+  res.send(form);
 });
+
+app.post('/SDC_Form', async (req, res, next) => {
+  console.log(req.body);
+  const procedureID = req.query.procedureID;
+  if(!req.body){
+    next(new Error("No XML Form in body"));
+  }
+  if(procedureID /* is in database */){
+    //set the form version on the database and change the id possibly?
+  }
+  //const formJSON = parseXMLSomehow(req.body);
+  //put formJSON into database;
+  res.send({
+    status: "blah blah",
+    somethingsomething: "Blah",
+  });
+})
+
+app.delete('/SDC_Form', async (req, res, next) => {
+  const procedureID = req.query.procedureID;
+  if(!procedureID){
+    next(new Error("procedureID did not show in parameters"));
+  }
+  //find form with procedureID in database and delete it
+  res.send("DELETED");
+})
+
+//default routes to test functionality. delete when done with
 app.get('/test', (req, res) => {
     res.send('Welcome to the backend!');
-})
+});
 app.post('/api/world', (req, res) => {
   console.log(req.body);
   res.send(
