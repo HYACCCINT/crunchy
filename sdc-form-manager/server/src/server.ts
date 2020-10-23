5707a9892f81edafd20666e39c43c0c053897b5a
Siyi Wang <hyacccint@gmail.com>
2020-10-23 13:11:32 -0600

    Change function format

--- sdc-form-manager/server/src/server.ts
+++ sdc-form-manager/server/src/server.ts
@@ -21,7 +21,7 @@ app.use('/graphql',cors(), graphqlHTTP({
 	graphiql: true
 }));
 app.use('/', router);
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
