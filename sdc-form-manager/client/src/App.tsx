6fce4a7593f3af40cda2b22040bb332458c3796e
Siyi Wang <hyacccint@gmail.com>
2020-10-25 14:55:13 -0600

    Remove imports of examplar

--- sdc-form-manager/client/src/App.tsx
+++ sdc-form-manager/client/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import { Provider as UrqlProvider } from 'urql';
 import { urqlClient } from './query';
-import {DBExample, MainPage} from './routes';
+import {MainPage} from './routes';
 // import axios from 'axios';
 
 import './App.css';
@@ -13,7 +13,6 @@ export const App = () => (
     <div className='app'>
       <UrqlProvider value={urqlClient}>
         <Route path='/' component={MainPage} exact/>
-        <Route path='/dbexample' component={DBExample} exact/>
       </UrqlProvider>
     </div>
   </Router>
