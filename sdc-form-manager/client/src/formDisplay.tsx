332c8be694bb932f2d0ab4ca395fb6bcc6112d91
Amritpal Aujla <amritpal.aujla@mail.utoronto.ca>
2020-10-30 15:49:41 -0400

    Update sdc-form-manager/client/src/formDisplay.tsx
    Co-authored-by: HYACCCINT <44329917+HYACCCINT@users.noreply.github.com>

--- sdc-form-manager/client/src/formDisplay.tsx
+++ sdc-form-manager/client/src/formDisplay.tsx
@@ -90,7 +90,7 @@ function renderProperties(data: any) {
   )
 }
 
-function getTempData() {
+const getTempData = () => {
   return [{
     "ProcedureId": "d290f1ee-6c54-4b01-90e6-d701748f0851",
     "Sections": [{
