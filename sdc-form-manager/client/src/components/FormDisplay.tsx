ed12f12f981cf3152680c22b12fb8a68a13539a8
Spyguy001 <amritpal.aujla@mail.utoronto.ca>
2020-11-06 13:34:22 -0500

    fixed form display using db data

--- sdc-form-manager/client/src/components/FormDisplay.tsx
+++ sdc-form-manager/client/src/components/FormDisplay.tsx
@@ -52,13 +52,13 @@ const renderQuestions = (questions: any) => {
   return (
     <div>
       {questions.map((question: any) => {
-        if (question === null || !question.isEnabled) return null;
+        if (question === null || question.isEnabled === false) return null;
         return (
           <div>
             <h4 className="blue-heading">{question.name}: {question.title} {question.mustImplement ? "*" : ""}</h4>
             {question.questionType === "text" ? (<input type="text"/>) : null}
             {question.questionType === "number" ? (<input type="number"></input>) : null}
-            {question.questionType === "single choice" ? (
+            {question.questionType.includes("single choice") ? (
               question.response.choices.map((choice: any) => {
                 return <p className="questionp"><input type="radio" name="q1" value="1"></input>{choice.name}: {choice.title}</p>;
               })
@@ -88,7 +88,7 @@ const renderProperties = (data: any) => {
       <h6>Version: {data.lastModified}</h6>
       {/*<h5>Release Date: {(new Date(data.releaseDate)).toLocaleString()}</h5>*/}
       <h6>Lineage: {data.lineage}</h6>
-      {data.patientId !== "template" ? <h5>Patient: {data.PatientId}</h5> : null}
+      {data.patientID !== "template" ? <h5>Patient: {data.patientID}</h5> : null}
     </div>
   )
 }
@@ -106,285 +106,14 @@ const assembleSection = (sectionId: string, data: any): any => {
   let {subSectionIDs, ...section} = sectionObj;
   section.subSections = [];
   subSectionIDs.forEach((id: string) => section.subSections.push(assembleSection(id, data)));
+  let questions = data.filter((item: any) => item.docType === "SDCQuestion" && item.superSectionID === sectionId);
   section.questions = [];
+  questions.forEach((question: any) => section.questions.push(assembleQuestions(question, data)));
   return section;
 }
 
-const getTempData = () => {
-  return [{
-    "ProcedureId": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-    "Sections": [{
-      "MustImplement": true,
-      "Subsections": [null, null],
-      "Title": "Title",
-      "Questions": [{
-        "TextAfterResponse": "TextAfterResponse",
-        "IsEnabled": true,
-        "Title": "Title",
-        "Properties": [null, null],
-        "Name": "Name",
-        "MustImplement": true,
-        "Type": "text",
-        "Answer": {
-          "Attributes": {
-            "FormResponseId": 2.027123023002322,
-            "QuestionId": 4.145608029883936
-          },
-          "UserInput": ""
-        },
-        "MaxCard": 9,
-        "Questionid": 3.616076749251911,
-        "MinCard": 7,
-        "SectionId": 2.3021358869347655,
-        "SubQuestions": [null, null]
-      }, {
-        "TextAfterResponse": "TextAfterResponse",
-        "IsEnabled": true,
-        "Title": "Title",
-        "Properties": [null, null],
-        "Name": "Name",
-        "MustImplement": true,
-        "Type": "text",
-        "Answer": {
-          "Attributes": {
-            "FormResponseId": 2.027123023002322,
-            "QuestionId": 4.145608029883936
-          },
-          "UserInput": ""
-        },
-        "MaxCard": 9,
-        "Questionid": 3.616076749251911,
-        "MinCard": 7,
-        "SectionId": 2.3021358869347655,
-        "SubQuestions": [null, null]
-      }],
-      "ID": "ID",
-      "Mincard": 5,
-      "Maxcard": 5,
-      "Name": "Name"
-    }, {
-      "MustImplement": true,
-      "Subsections": [null, null],
-      "Title": "Title",
-      "Questions": [{
-        "TextAfterResponse": "TextAfterResponse",
-        "IsEnabled": true,
-        "Title": "Title",
-        "Properties": [null, null],
-        "Name": "Name",
-        "MustImplement": true,
-        "Type": "text",
-        "Answer": {
-          "Attributes": {
-            "FormResponseId": 2.027123023002322,
-            "QuestionId": 4.145608029883936
-          },
-          "UserInput": ""
-        },
-        "MaxCard": 9,
-        "Questionid": 3.616076749251911,
-        "MinCard": 7,
-        "SectionId": 2.3021358869347655,
-        "SubQuestions": [null, null]
-      }, {
-        "TextAfterResponse": "TextAfterResponse",
-        "IsEnabled": true,
-        "Title": "Title",
-        "Properties": [null, null],
-        "Name": "Name",
-        "MustImplement": true,
-        "Type": "single choice",
-        "Answer": {
-          "Attributes": {
-            "FormResponseId": 2.027123023002322,
-            "QuestionId": 4.145608029883936
-          },
-          "Choices": [
-            {
-              "Name": "LI_NS_76240",
-              "ID": "76240.100004300",
-              "Title": "LDCT"
-            },
-            {
-              "Name": "LI_Oth_76239",
-              "ID": "76239.100004300",
-              "Title": "Other (specify)",
-              "TextItem": true
-            }
-          ],
-          "UserInput": ""
-        },
-        "MaxCard": 9,
-        "Questionid": 3.616076749251911,
-        "MinCard": 7,
-        "SectionId": 2.3021358869347655,
-        "SubQuestions": [null, null]
-      }],
-      "ID": "ID",
-      "Mincard": 5,
-      "Maxcard": 5,
-      "Name": "Name"
-    }],
-    "releaseDate": "2016-08-29T09:12:33.001Z",
-    "MetaProperties": {
-      "Version": 0.8008281904610115,
-      "Title": "Appendix form",
-      "URI": "URI",
-      "properties": {
-        "Order": 6.027456183070403,
-        "Val": "Val",
-        "Type": "Type",
-        "PropName": "PropName",
-        "Propclass": "Propclass",
-        "Name": "Name"
-      }
-    },
-    "PatientId": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-    "name": "Widget Adapter",
-    "Footer": "Footer",
-    "Contact": {
-      "emails": [{
-        "Name": "Name"
-      }, {
-        "Name": "Name"
-      }],
-      "OrganizationNmae": "OrganizationNmae"
-    },
-    "BodyProperties": {
-      "Title": "Title",
-      "Id": 1.4658129805029452,
-      "Properties": [null, null]
-    }
-  }, {
-    "ProcedureId": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-    "Sections": [{
-      "MustImplement": true,
-      "Subsections": [null, null],
-      "Title": "Title",
-      "Questions": [{
-        "TextAfterResponse": "TextAfterResponse",
-        "IsEnabled": true,
-        "Title": "Title",
-        "Properties": [null, null],
-        "Name": "Name",
-        "MustImplement": true,
-        "Type": "text",
-        "Answer": {
-          "Attributes": {
-            "FormResponseId": 2.027123023002322,
-            "QuestionId": 4.145608029883936
-          },
-          "UserInput": ""
-        },
-        "MaxCard": 9,
-        "Questionid": 3.616076749251911,
-        "MinCard": 7,
-        "SectionId": 2.3021358869347655,
-        "SubQuestions": [null, null]
-      }, {
-        "TextAfterResponse": "TextAfterResponse",
-        "IsEnabled": true,
-        "Title": "Title",
-        "Properties": [null, null],
-        "Name": "Name",
-        "MustImplement": true,
-        "Type": "text",
-        "Answer": {
-          "Attributes": {
-            "FormResponseId": 2.027123023002322,
-            "QuestionId": 4.145608029883936
-          },
-          "UserInput": ""
-        },
-        "MaxCard": 9,
-        "Questionid": 3.616076749251911,
-        "MinCard": 7,
-        "SectionId": 2.3021358869347655,
-        "SubQuestions": [null, null]
-      }],
-      "ID": "ID",
-      "Mincard": 5,
-      "Maxcard": 5,
-      "Name": "Name"
-    }, {
-      "MustImplement": true,
-      "Subsections": [null, null],
-      "Title": "Title",
-      "Questions": [{
-        "TextAfterResponse": "TextAfterResponse",
-        "IsEnabled": true,
-        "Title": "Title",
-        "Properties": [null, null],
-        "Name": "Name",
-        "MustImplement": true,
-        "Type": "number",
-        "Answer": {
-          "Attributes": {
-            "FormResponseId": 2.027123023002322,
-            "QuestionId": 4.145608029883936
-          },
-          "UserInput": 0
-        },
-        "MaxCard": 9,
-        "Questionid": 3.616076749251911,
-        "MinCard": 7,
-        "SectionId": 2.3021358869347655,
-        "SubQuestions": [null, null]
-      }, {
-        "TextAfterResponse": "TextAfterResponse",
-        "IsEnabled": true,
-        "Title": "Title",
-        "Properties": [null, null],
-        "Name": "Name",
-        "MustImplement": true,
-        "Type": "text",
-        "Answer": {
-          "Attributes": {
-            "FormResponseId": 2.027123023002322,
-            "QuestionId": 4.145608029883936
-          },
-          "UserInput": ""
-        },
-        "MaxCard": 9,
-        "Questionid": 3.616076749251911,
-        "MinCard": 7,
-        "SectionId": 2.3021358869347655,
-        "SubQuestions": [null, null]
-      }],
-      "ID": "ID",
-      "Mincard": 5,
-      "Maxcard": 5,
-      "Name": "Name"
-    }],
-    "releaseDate": "2016-08-29T09:12:33.001Z",
-    "MetaProperties": {
-      "Version": 0.8008281904610115,
-      "Title": "Appendix form",
-      "URI": "URI",
-      "properties": {
-        "Order": 6.027456183070403,
-        "Val": "Val",
-        "Type": "Type",
-        "PropName": "PropName",
-        "Propclass": "Propclass",
-        "Name": "Name"
-      }
-    },
-    "PatientId": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-    "name": "Widget Adapter",
-    "Footer": "Footer",
-    "Contact": {
-      "emails": [{
-        "Name": "Name"
-      }, {
-        "Name": "Name"
-      }],
-      "OrganizationNmae": "OrganizationNmae"
-    },
-    "BodyProperties": {
-      "Title": "Title",
-      "Id": 1.4658129805029452,
-      "Properties": [null, null]
-    }
-  }]
+const assembleQuestions = (question: any, data: any): any => {
+  const subQuestions = data.filter((item: any) => item.docType === "SDCQuestion" && item.superQuestionID === question.id);
+  question.subQuestions = subQuestions;
+  return question;
 }
