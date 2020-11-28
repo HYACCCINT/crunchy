607442c31fee0e5f283b9e1954eba4f333da9563
Spyguy001 <amritpal.aujla@mail.utoronto.ca>
2020-11-27 23:44:20 -0500

    added arbitrary properties to questions

--- client/src/form-filler/FormFill.tsx
+++ client/src/form-filler/FormFill.tsx
@@ -68,9 +68,9 @@ export const FormFill = () => {
         <h6>Lineage: {data.lineage}</h6>
         {data.patientID !== "template" ? <h5>Patient: {data.patientID}</h5> : null}
         {data.arbitraryProperties ? data.arbitraryProperties.map((item: any) => {
-          item ? <div className="arbitraryProperties">{
-            Object.entries(item).map((entry: any) => <h6 className="arbitraryProperty">{JSON.stringify(entry[0])}: {JSON.stringify(entry[1])}</h6>)
-          }</div>
+          item ? <div className="arbitraryFormProperties">{
+            Object.entries(item).map((entry: any) => <h6 className="arbitraryFormProperty">{JSON.stringify(entry[0])}: {JSON.stringify(entry[1])}</h6>)
+          }</div> : ""
         }) : ""}
       </div>
     )
